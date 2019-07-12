126f17f43daf597de939e1f2105fe56522261993
Joao Coelho <joaocoelho93@gmail.com>
2019-07-11 22:38:19 -0400

    minor adjustments

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,26 +2,20 @@ require 'json'
 
 # Possible Conditions on Query:
 # Name filter
-# offset -> for pagination
-# sort Total Rushing Yards, Longest Rush and Total Rushing Touchdowns
+# offset for pagination
+# Sorting for Total Rushing Yards, Longest Rush and Total Rushing Touchdowns
 class PlayersController < ApplicationController
   def index
 
     # Parse Players JSON file
     full_player_list = JSON.parse(File.read('app/rushing.json'));
 
-    filtered_player_list = []
+    filtered_player_list = full_player_list
 
-    # Extract Potential Parameter Values
-    offset = params[:offset].to_i
+    # Extract Potential Query String Values
+    offset = params[:offset]
     name_search = params[:name_search]
     sort_by = params[:sort_by]
-
-    # If the offset is greater than the amount of players in the first place,
-    # simply set it to 0
-    if offset >= full_player_list.length
-      offset = 0
-    end
 
     # Perform Name Search
     if name_search
@@ -32,10 +26,18 @@ class PlayersController < ApplicationController
       end
     end
 
-    #
-    filtered_player_list = filtered_player_list[offset...offset + 25] || []
+    if offset
+      # Parse the offset into a usable integer
+      offset = offset.to_i
+      # If the offset is greater or equal to the amount of players in the first place,
+      # simply set it to 0
+      if offset >= full_player_list.length
+        offset = 0
+      end
+      # Return 25 records, starting from the offset value
+      filtered_player_list = filtered_player_list[offset...offset + 25] || []
+    end
 
-    # Return 25 Players, starting from the offset value
     return render :json => filtered_player_list
   end
 end
