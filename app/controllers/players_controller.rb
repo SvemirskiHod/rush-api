6966c1c51466f06f5ef997c1023a52aecda5c2c6
Joao Coelho <joaocoelho93@gmail.com>
2019-07-11 22:27:32 -0400

    Players endpoint now returns players with offset value and name search

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,41 @@
 require 'json'
 
+# Possible Conditions on Query:
+# Name filter
+# offset -> for pagination
+# sort Total Rushing Yards, Longest Rush and Total Rushing Touchdowns
 class PlayersController < ApplicationController
   def index
-    full_list = JSON.parse(File.read('app/rushing.json'));
-    render :json => full_list
+
+    # Parse Players JSON file
+    full_player_list = JSON.parse(File.read('app/rushing.json'));
+
+    filtered_player_list = []
+
+    # Extract Potential Parameter Values
+    offset = params[:offset].to_i
+    name_search = params[:name_search]
+    sort_by = params[:sort_by]
+
+    # If the offset is greater than the amount of players in the first place,
+    # simply set it to 0
+    if offset >= full_player_list.length
+      offset = 0
+    end
+
+    # Perform Name Search
+    if name_search
+      full_player_list.each do |player|
+        if player['Player'].downcase.include? name_search.downcase
+          filtered_player_list.push(player)
+        end
+      end
+    end
+
+    #
+    filtered_player_list = filtered_player_list[offset...offset + 25] || []
+
+    # Return 25 Players, starting from the offset value
+    return render :json => filtered_player_list
   end
 end
